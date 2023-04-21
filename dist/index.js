f2d90cb2d11018ce97215fc39e70b20de4e27095
celina-ijs <celina.dev@instajs.com>
2023-04-21 16:30:22 +0700

    Update code to use slot

--- dist/index.js
+++ dist/index.js
@@ -4913,17 +4913,18 @@ define("@scom/scom-page-builder", ["require", "exports", "@ijstech/components", 
                 elements: [element]
             };
             if (module.path === 'scom-nft-minter' || module.path === 'scom-gem-token') {
-                element.module = index_64.getDappContainer();
+                // { ...module, localPath: `libs/@scom/${module.path}` }
+                console.log(module);
+                element.module = module;
                 element.columnSpan = 6;
                 element.properties = {
-                    networks: [43113],
-                    wallets: ["metamask"],
-                    content: {
-                        module: Object.assign(Object.assign({}, module), { localPath: `libs/@scom/${module.path}` }),
-                        properties: {
-                            width: '100%'
-                        }
-                    }
+                    networks: [{
+                            chainId: 43113
+                        }],
+                    wallets: [{
+                            name: "metamask"
+                        }],
+                    width: '100%'
                 };
             }
             return await this.pageRows.appendRow(rowData);
