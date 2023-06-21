33927e9dcc115d14bb75f8cf55c5aa209460f877
ansonwcy <cywongci@connect.ust.hk>
2023-06-21 13:40:49 +0800

    enhance selection UI on composite section

--- src/page/pageSection.css.ts
+++ src/page/pageSection.css.ts
@@ -10,8 +10,13 @@ Styles.cssRule('ide-section', {
     maxWidth: '100%',
     border: '2px solid transparent',
     $nest: {
-        '&.active, &:focus': {
-            border: `2px solid ${Theme.colors.primary.main}`,
+        // '&.active, &:focus': {
+        //     border: `2px solid ${Theme.colors.primary.main}`,
+        //     transition: 'border ease-in .2s'
+        // },
+        '&:hover .section-border': {
+            display: 'block',
+            outline: `2px solid ${Theme.colors.primary.main}`,
             transition: 'border ease-in .2s'
         },
         'h1, h2, h3, h4, h5, h6': {
