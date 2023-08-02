0c040bf30ebb3fe7fc4f42a4a6f09ece75d8ea7c
celina-ijs <celina.dev@instajs.com>
2023-08-02 15:57:20 +0700

    The relative position of mouse and dragging blue frame should remain unchanged when dragging

--- dist/index.js
+++ dist/index.js
@@ -4348,9 +4348,7 @@ define("@scom/scom-page-builder/page/pageRow.tsx", ["require", "exports", "@ijst
                 self.toggleUI(!!((_b = (_a = self.data) === null || _a === void 0 ? void 0 : _a.elements) === null || _b === void 0 ? void 0 : _b.length));
             }
             function dragEnter(enterTarget, clientX, clientY, collision) {
-                var _a, _b, _c;
-                if (!enterTarget)
-                    return;
+                var _a, _b, _c, _d, _e;
                 if (!enterTarget || !self.currentElement)
                     return;
                 if (enterTarget.closest('#pnlEmty')) {
@@ -4370,28 +4368,45 @@ define("@scom/scom-page-builder/page/pageRow.tsx", ["require", "exports", "@ijst
                 self.addDottedLines();
                 toggleAllToolbarBoarder(true);
                 if (target) {
-                    const column = Number(target.dataset.column);
+                    const dropRow = target.closest('ide-row');
+                    let offsetLeft = 0;
+                    if (!((_a = (0, index_41.getDragData)()) === null || _a === void 0 ? void 0 : _a.module)) {
+                        const dragRow = self.currentElement.closest('ide-row');
+                        if ((dragRow === null || dragRow === void 0 ? void 0 : dragRow.id) && (dropRow === null || dropRow === void 0 ? void 0 : dropRow.id) && dragRow.id === dropRow.id) {
+                            offsetLeft = Math.floor((startX + index_40.GAP_WIDTH) / (self.gridColumnWidth + index_40.GAP_WIDTH));
+                        }
+                    }
+                    const targetCol = Number(target.dataset.column);
+                    const column = targetCol - offsetLeft > 0 ? targetCol - offsetLeft : targetCol;
                     const columnSpan = self.currentElement.dataset.columnSpan
                         ? Number(self.currentElement.dataset.columnSpan)
                         : index_40.INIT_COLUMN_SPAN;
-                    const colSpan = Math.min(columnSpan, self.maxColumn);
-                    const colStart = Math.min(column, self.maxColumn - colSpan + 1);
-                    const grid = target.closest('.grid');
-                    const sections = Array.from(grid === null || grid === void 0 ? void 0 : grid.querySelectorAll('ide-section'));
-                    const sortedSections = sections.sort((a, b) => Number(a.dataset.column) - Number(b.dataset.column));
+                    let colSpan = Math.min(columnSpan, self.maxColumn);
+                    let colStart = Math.min(column, self.maxColumn);
+                    const sections = Array.from(dropRow === null || dropRow === void 0 ? void 0 : dropRow.querySelectorAll('ide-section'));
+                    const sortedSections = sections.sort((a, b) => Number(b.dataset.column) - Number(a.dataset.column));
                     let spaces = 0;
                     let findedSection = null;
+                    let isUpdated = false;
                     for (let i = 0; i < sortedSections.length; i++) {
                         const section = sortedSections[i];
                         const sectionColumn = Number(section.dataset.column);
                         const sectionColumnSpan = Number(section.dataset.columnSpan);
+                        const sectionData = sectionColumn + sectionColumnSpan;
+                        console.log(colStart, self.maxColumn - colStart < colSpan, colSpan);
+                        if (colStart >= sectionData && (self.maxColumn - colStart) + 1 < colSpan && !isUpdated) {
+                            colStart = sectionData;
+                            isUpdated = true;
+                        }
                         const colData = colStart + colSpan;
-                        if (colStart >= sectionColumn && colData <= sectionColumn + sectionColumnSpan) {
+                        if ((colStart >= sectionColumn && colData <= sectionData) || (colStart < sectionData && colData > sectionData)) {
                             findedSection = section;
                         }
-                        spaces += sectionColumnSpan;
+                        if (((_b = self.currentElement) === null || _b === void 0 ? void 0 : _b.id) !== section.id) {
+                            spaces += sectionColumnSpan;
+                        }
                     }
-                    if (findedSection && findedSection != self.currentElement) {
+                    if (findedSection && findedSection !== self.currentElement || index_40.MAX_COLUMN - spaces < 1) {
                         removeRectangles();
                         return;
                     }
@@ -4418,9 +4433,9 @@ define("@scom/scom-page-builder/page/pageRow.tsx", ["require", "exports", "@ijst
                                 updateClass(topBlock, 'is-dragenter');
                             }
                         }
-                        const curElmCol = Number((_a = section === null || section === void 0 ? void 0 : section.dataset) === null || _a === void 0 ? void 0 : _a.column);
-                        const curElmColSpan = Number((_b = section === null || section === void 0 ? void 0 : section.dataset) === null || _b === void 0 ? void 0 : _b.columnSpan);
-                        const sections = Array.from((_c = section.closest('#pnlRow')) === null || _c === void 0 ? void 0 : _c.querySelectorAll('ide-section'));
+                        const curElmCol = Number((_c = section === null || section === void 0 ? void 0 : section.dataset) === null || _c === void 0 ? void 0 : _c.column);
+                        const curElmColSpan = Number((_d = section === null || section === void 0 ? void 0 : section.dataset) === null || _d === void 0 ? void 0 : _d.columnSpan);
+                        const sections = Array.from((_e = section.closest('#pnlRow')) === null || _e === void 0 ? void 0 : _e.querySelectorAll('ide-section'));
                         const nextElm = sections.find((el) => {
                             const column = Number(el.dataset.column);
                             return !isNaN(column) && curElmCol + curElmColSpan === column;
@@ -4750,7 +4765,12 @@ define("@scom/scom-page-builder/page/pageRow.tsx", ["require", "exports", "@ijst
                 const config = { id: (0, index_43.generateUUID)() };
                 // check if drop on a fixed-panel
                 if (nearestFixedItem) {
-                    const column = Number(nearestFixedItem.dataset.column);
+                    const offsetLeft = Math.floor((startX + index_40.GAP_WIDTH) / (self.gridColumnWidth + index_40.GAP_WIDTH));
+                    let column = Number(nearestFixedItem.dataset.column);
+                    if (column - offsetLeft > 0) {
+                        nearestFixedItem = pageRow.querySelector(`.fixed-grid-item[data-column='${column - offsetLeft}']`);
+                    }
+                    column = Number(nearestFixedItem.dataset.column);
                     const columnSpan = self.currentElement.dataset.columnSpan
                         ? Number(self.currentElement.dataset.columnSpan)
                         : index_40.INIT_COLUMN_SPAN;
@@ -6948,7 +6968,7 @@ define("@scom/scom-page-builder/page/pageSidebar.tsx", ["require", "exports", "@
         }
         render() {
             return (this.$render("i-hstack", { position: 'fixed', top: '50%', right: 24, height: 0, width: 0, verticalAlignment: 'center' },
-                this.$render("i-vstack", { position: 'absolute', right: "0px" },
+                this.$render("i-vstack", { position: 'absolute', right: "0px", zIndex: 100 },
                     this.$render("i-vstack", { id: 'toolbars', class: pageSidebar_css_1.categoryPanelStyle, gap: "0.25rem", margin: { bottom: '1rem' } }),
                     this.$render("i-vstack", { id: 'pnlWidgetCategory', class: pageSidebar_css_1.categoryPanelStyle, gap: "0.25rem" })),
                 this.$render("i-modal", { id: 'mdWidget', class: pageSidebar_css_1.widgetModalStyle, height: 'auto', width: 320, maxHeight: '80vh', showBackdrop: false, popupPlacement: 'left' },
