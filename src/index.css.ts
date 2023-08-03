a269971a090f04fd1a49b9d118250a00151d15ed
MikeYuen <nhatminh.150596@gmail.com>
2023-08-03 14:42:34 +0700

    fix bgImage for page settings

--- src/index.css.ts
+++ src/index.css.ts
@@ -6,6 +6,9 @@ Styles.cssRule('#editor', {
   $nest: {
     '.pnl-editor-wrapper': {
       display: 'block',
+      backgroundRepeat: 'repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
       boxShadow: 'rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px',
     },
     '.custom-input input': {
